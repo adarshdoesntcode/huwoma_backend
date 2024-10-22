6ba4fd0525c04d2aba6ddd9ef6ab21966e42252f
Adarsh Das <adarsh.191605@ncit.edu.np>
2024-10-22 10:26:54 +0545

    cors updated

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,4 +1,4 @@
 //creating whiteList in order to ensure your server is accessed by limited domain
-const allowedOrigins = ["http://localhost:5173", "https://huwoma.vercel.app/"];
+const allowedOrigins = ["http://localhost:5173", "https://huwoma.vercel.app"];
 
 module.exports = allowedOrigins;
