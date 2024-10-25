1e18285141bbb273db54a7169b69ecf65ac777d8
Adarsh Das <adarsh.191605@ncit.edu.np>
2024-10-25 10:31:52 +0545

    added mongodb connection options

--- config/dbConn.js
+++ config/dbConn.js
@@ -2,7 +2,12 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.DATABASE_URI);
+    await mongoose.connect(process.env.DATABASE_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      tls: true,
+      serverSelectionTimeoutMS: 5000,
+    });
   } catch (err) {
     console.error(err);
   }
