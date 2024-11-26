4033f6a18423b5bdf26aeda60fc25fe2de2dd350
Adarsh Das <adarsh.191605@ncit.edu.np>
2024-11-26 19:17:41 +0545

    upstash->redis cloud

--- config/redisConn.js
+++ config/redisConn.js
@@ -1,9 +1,9 @@
 const Redis = require("ioredis");
 
-const redis = new Redis(process.env.REDIS_ACCESS_URL);
-
-redis.on("command", (cmd) => {
-  console.log("Redis Command:", cmd.name, cmd.args);
+const redis = new Redis({
+  host: process.env.REDIS_ACCESS_URL,
+  port: process.env.REDIS_PORT,
+  password: process.env.REDIS_PASSWORD,
 });
 
 module.exports = redis;
